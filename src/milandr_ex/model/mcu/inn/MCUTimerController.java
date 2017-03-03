72b1b045ef1c82e76f0899f730add9e3884229f4
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-03 10:23:59 +0300

    fill timer block props by McuBlockProperty

--- src/milandr_ex/model/mcu/inn/MCUTimerController.java
+++ src/milandr_ex/model/mcu/inn/MCUTimerController.java
@@ -1,18 +1,34 @@
 package milandr_ex.model.mcu.inn;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import milandr_ex.data.AppScene;
 import milandr_ex.data.Device;
+import milandr_ex.data.McuBlockProperty;
 import milandr_ex.model.BasicController;
 
+import static milandr_ex.data.McuBlockProperty.div128List;
+import static milandr_ex.data.McuBlockProperty.div512List;
+import static milandr_ex.data.McuBlockProperty.unitList;
+
 public class MCUTimerController extends BasicController {
+
+	@FXML
+	private GridPane tmr_grid;
+
+	@Override
+	protected Pane getPropControl() {
+		return tmr_grid;
+	}
 
 	@Override
 	protected void postInit(AppScene scene) {
 		setDevicePair(Device.EPairNames.TMR);
-	}
-
-	@Override
-	protected void initLater(AppScene scene) {
-		super.initLater(scene);
+		getDevicePair().model().setBundle(getMessages());
+		getDevicePair().model().addModelProp(McuBlockProperty.getC("pre_div", div128List));
+		getDevicePair().model().addModelProp(McuBlockProperty.get("tim_clk", ""));
+		getDevicePair().model().addModelProp(McuBlockProperty.getF("freq_div", div512List, unitList),
+				"f_div.main_div", "f_time.time_freq", "f_units.tf_units");
 	}
 }
