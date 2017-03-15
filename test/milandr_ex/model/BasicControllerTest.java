fefe49b23404100371cbed890c8fcc310c079552
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-15 10:53:16 +0300

    move initController and testCodeResult to BasicControllerTest for re-usage in future tests

--- test/milandr_ex/model/BasicControllerTest.java
+++ test/milandr_ex/model/BasicControllerTest.java
@@ -1,7 +1,59 @@
 package milandr_ex.model;
 
+import milandr_ex.data.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by lizard2k1 on 15.03.2017.
  */
 public class BasicControllerTest {
+	@Mock
+	protected AppScene appScene;
+	@Mock
+	protected PinoutsModel pinoutsModel;
+	@Mock
+	protected McuBlockModel blockModel;
+	@Mock
+	protected ClockModel clockModel;
+	protected Device device;
+
+	protected void initController(BasicController controller) {
+		MockitoAnnotations.initMocks(this);
+		device = DeviceFactory.getDevice("LQFP64");
+		CodeGenerator codeGenerator = CodeGenerator.instance();
+		when(appScene.getPinoutsModel()).thenReturn(pinoutsModel);
+		when(appScene.genKind()).thenReturn(CodeGenerator.GenKind.MODEL);
+		when(appScene.getCodeGenerator()).thenReturn(codeGenerator);
+		when(pinoutsModel.getBlockModel(Mockito.anyString())).thenReturn(blockModel);
+		when(pinoutsModel.getClockModel()).thenReturn(clockModel);
+		controller.setScene(appScene);
+		controller.postInit(appScene);
+	}
+
+	protected void testCodeResult(String expectedCodeResult, int expectedComments, int expectedLines,
+								  int expectedSize, List<String> newCodeList) {
+		StringBuilder codeResult = new StringBuilder();
+		int commentsCount = 0;
+		int linesCount = 0;
+		for(String codeLine: newCodeList) {
+			if (codeLine.trim().contains("//")) {
+				commentsCount++;
+				codeLine = codeLine.substring(0, codeLine.indexOf("//"));
+			}
+			if (codeLine.trim().isEmpty()) continue;
+			linesCount++;
+			codeResult.append(codeLine.trim().replaceAll("\\s", ""));
+		}
+		assertEquals(expectedComments, commentsCount);
+		assertEquals(expectedLines, linesCount);
+		assertEquals(expectedSize, newCodeList.size());
+		assertEquals(expectedCodeResult, codeResult.toString());
+	}
 }
--- test/milandr_ex/model/mcu/inn/MCUPowerControllerTest.java
+++ test/milandr_ex/model/mcu/inn/MCUPowerControllerTest.java
@@ -1,52 +1,28 @@
 package milandr_ex.model.mcu.inn;
 
 import com.google.common.collect.Lists;
-import milandr_ex.data.*;
+import milandr_ex.model.BasicControllerTest;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import static org.junit.Assert.*;
 
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Test class for verify result of code generation for Power block
  * Created by lizard2k1 on 15.03.2017.
  */
-public class MCUPowerControllerTest {
+public class MCUPowerControllerTest extends BasicControllerTest {
 	private static String defaultCodeResult = "voidPWR_init(void){MDR_RST_CLK->PER_CLOCK|=((1<<11));MDR_POWER->PVDCS=((0<<3)|(0<<1));}";
 
-	@Mock
-	private AppScene appScene;
-	@Mock
-	private PinoutsModel pinoutsModel;
-	@Mock
-	private McuBlockModel blockModel;
-	@Mock
-	private ClockModel clockModel;
-	private Device device;
-
-	private CodeGenerator codeGenerator;
 	private MCUPowerController powerController;
 
 	@Before
 	public void initController() {
-		MockitoAnnotations.initMocks(this);
-		codeGenerator = CodeGenerator.instance();
 		powerController = new MCUPowerController();
-		powerController.setScene(appScene);
-		powerController.postInit(appScene);
-		device = DeviceFactory.getDevice("LQFP64");
-		when(appScene.getPinoutsModel()).thenReturn(pinoutsModel);
-		when(appScene.genKind()).thenReturn(CodeGenerator.GenKind.MODEL);
-		when(appScene.getCodeGenerator()).thenReturn(codeGenerator);
-		when(pinoutsModel.getBlockModel(Mockito.anyString())).thenReturn(blockModel);
-		when(pinoutsModel.getClockModel()).thenReturn(clockModel);
+		super.initController(powerController);
 	}
 	@Test
 	public void testDefaultPowerParams() {
@@ -54,21 +30,6 @@ public class MCUPowerControllerTest {
 		assertEquals("[]", String.valueOf(oldCodeList));
 		List<String> newCodeList = powerController.generateCode(device, oldCodeList);
 		assertNotEquals(oldCodeList, newCodeList);
-		StringBuilder codeResult = new StringBuilder();
-		int commentsCount = 0;
-		int linesCount = 0;
-		for(String codeLine: newCodeList) {
-			if (codeLine.trim().contains("//")) {
-				commentsCount++;
-				codeLine = codeLine.substring(0, codeLine.indexOf("//"));
-			}
-			if (codeLine.trim().isEmpty()) continue;
-			linesCount++;
-			codeResult.append(codeLine.trim().replaceAll("\\s", ""));
-		}
-		assertEquals(6, commentsCount);
-		assertEquals(5, linesCount);
-		assertEquals(12, newCodeList.size());
-		assertEquals(defaultCodeResult, codeResult.toString());
+		testCodeResult(defaultCodeResult, 6, 5, 12, newCodeList);
 	}
 }
