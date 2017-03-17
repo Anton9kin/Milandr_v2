572bc370c889ec28aaf741c4728720b431a0c91a
anton <adeviatkin@intech-global.com>
2017-03-17 11:13:41 +0300

    if adc_clk not choose in any block adc (adc1 or adc2) don't generate ADC_MCO_CLOCK

--- src/milandr_ex/model/mcu/inn/MCUAdcController.java
+++ src/milandr_ex/model/mcu/inn/MCUAdcController.java
@@ -114,10 +114,16 @@ public class MCUAdcController extends BasicController {
 				int adcC2 = getClockProp("ADC-C2.S");
 				int adcDiv = getClockProp("ADC-C2-O.S");
 
-				MDR_RST_CLK.get();
-				MDR_RST_CLK.set(
-						Param.ADC_MCO_CLOCK.set(adcC1, adcC2, adcDiv, 1).shift(0, 4, 8, 13)
-					).pre(1, 1, 2, 4).cmt("ADC_C1", "ADC_C2", "ADC_C3", "ADC_CLK").build(oldCode);
+				int adcCLKSrc = isCboxChecked(0) == true ? getConfPropInt("adc_src", 0) : 1; 
+				adcCLKSrc += isCboxChecked(1) == true ? getConfPropInt("adc_src", 1) : 1;
+				
+				if (adcCLKSrc != 2){
+				
+					MDR_RST_CLK.get();
+					MDR_RST_CLK.set(
+							Param.ADC_MCO_CLOCK.set(adcC1, adcC2, adcDiv, 1).shift(0, 4, 8, 13)
+							).pre(1, 1, 2, 4).cmt("ADC_C1", "ADC_C2", "ADC_C3", "ADC_CLK").build(oldCode);
+				}
 				break;
 			case 1:
 				MDR_RST_CLK.set(Param.PER_CLOCK.seti(1, 17, "|")).cmt("тактирование АЦП").build(oldCode);
