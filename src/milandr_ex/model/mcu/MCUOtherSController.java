40afbb3b63eee1b4e0fcd7db3257bb7eee6a5d09
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-18 22:13:39 +0300

    first code for additional features tab

---
+++ src/milandr_ex/model/mcu/MCUOtherSController.java
@@ -0,0 +1,27 @@
+package milandr_ex.model.mcu;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import milandr_ex.data.AppScene;
+import milandr_ex.model.BasicController;
+
+/**
+ * Main controller for additional features
+ * Created by lizard on 18.03.17 at 22:08.
+ */
+public class MCUOtherSController extends BasicController {
+
+	@FXML
+	private GridPane othr_grid;
+
+	@Override
+	protected Pane getPropControl() {
+		return othr_grid;
+	}
+
+	@Override
+	protected void postInit(AppScene scene) {
+
+	}
+}
