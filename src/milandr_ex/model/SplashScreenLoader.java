e873bc718bb0363087fe00de058810ca1929040b
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-18 14:57:07 +0300

    add progress to splash, makeFadeSplash

--- src/milandr_ex/model/SplashScreenLoader.java
+++ src/milandr_ex/model/SplashScreenLoader.java
@@ -1,14 +1,20 @@
 package milandr_ex.model;
 
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
 import javafx.application.Preloader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import milandr_ex.MilandrEx;
 import milandr_ex.utils.LoaderUtils;
 
 import static milandr_ex.utils.LoaderUtils.loadImage;
@@ -20,6 +26,10 @@ import static milandr_ex.utils.LoaderUtils.loadImage;
 public class SplashScreenLoader extends Preloader {
 	@FXML
 	private ImageView splashIm;
+	@FXML
+	private ProgressBar loadProgress;
+	@FXML
+	private Label progressText;
 	private Stage splashScreen;
 
 	@Override
@@ -29,9 +39,19 @@ public class SplashScreenLoader extends Preloader {
 		splashScreen.show();
 	}
 
+	private void makeFadeSplash(final Stage stage) {
+		Parent splashLayout = stage.getScene().getRoot();
+		FadeTransition fadeSplash = new FadeTransition(Duration.seconds(5), splashLayout);
+		fadeSplash.setFromValue(1.0);
+		fadeSplash.setToValue(0.0);
+		fadeSplash.setOnFinished(actionEvent -> stage.hide());
+		fadeSplash.play();
+	}
+
 	@FXML
 	private void initialize() {
 		loadImage(getClass(), splashIm, "milandr_logo2.jpg");
+		progressText.setText("Loading hobbits with pie . . .");
 	}
 
 	public Scene createScene() {
@@ -42,7 +62,13 @@ public class SplashScreenLoader extends Preloader {
 	@Override
 	public void handleApplicationNotification(PreloaderNotification notification) {
 		if (notification instanceof StateChangeNotification) {
-			splashScreen.hide();
+			Application app = ((StateChangeNotification) notification).getApplication();
+			if (app instanceof MilandrEx) {
+				MilandrEx mApp = (MilandrEx) app;
+				if (!mApp.getScene().isSetupInProcess()) {
+					makeFadeSplash(splashScreen);
+				}
+			}
 		}
 	}
 }
