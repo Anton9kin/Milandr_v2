b123c8d4f260f576fb094a44c374341c9b8cb767
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-20 18:17:32 +0300

    separate timer code with TMR and IRQ functions(methods) by code step

--- src/milandr_ex/model/mcu/inn/MCUTimerController.java
+++ src/milandr_ex/model/mcu/inn/MCUTimerController.java
@@ -41,69 +41,78 @@ public class MCUTimerController extends BasicController {
 		int TIM_ARR = 5;
 		int timTim = 6;
 		int timUnit = 7;
+		switch (codeStep) {
+			case 0:
                 /* 2 строка */
-		g().addCodeStr(oldCode, "    MDR_RST_CLK->PER_CLOCK |= 1 << " + (13 + TIM_CLK_EN) + ";");
-		g().addCodeStr(oldCode, "//разрешение тактирования Таймера " + TIM_CLK_EN);
+				g().addCodeStr(oldCode, "    MDR_RST_CLK->PER_CLOCK |= 1 << " + (13 + TIM_CLK_EN) + ";");
+				g().addCodeStr(oldCode, "//разрешение тактирования Таймера " + TIM_CLK_EN);
 
                 /* 3 строка */
-		switch (TIM_CLK_EN)
-		{
-			case 1:
-				g().addCodeStr(oldCode, " /* делитель тактовой частоты Таймера 1 */");
-				g().addCodeStr(oldCode, "    MDR_RST_CLK->TIM_CLOCK = (" + TIM_BRG);
-				g().addCodeStr(oldCode, " /* разешение тактирования Таймера 1 */");
-				g().addCodeStr(oldCode, "                            |(1 << 24)); ");
-				break;
-			case 2:
-				g().addCodeStr(oldCode, " /* делитель тактовой частоты Таймера 2 */");
-				g().addCodeStr(oldCode, "    MDR_RST_CLK->TIM_CLOCK = ((" + TIM_BRG + " << 8)");
-				g().addCodeStr(oldCode, " /* разешение тактирования Таймера 2 */");
-				g().addCodeStr(oldCode, "                            | (1 << 25)); ");
-				break;
-			case 3:
-				g().addCodeStr(oldCode, " /* делитель тактовой частоты Таймера 3 */");
-				g().addCodeStr(oldCode, "    MDR_RST_CLK->TIM_CLOCK = ((" + TIM_BRG + " << 16)");
-				g().addCodeStr(oldCode, " /* разешение тактирования Таймера 3 */");
-				g().addCodeStr(oldCode, "                            | (1 << 26)); ");
-				break;
-		}
+				switch (TIM_CLK_EN)
+				{
+					case 1:
+						g().addCodeStr(oldCode, " /* делитель тактовой частоты Таймера 1 */");
+						g().addCodeStr(oldCode, "    MDR_RST_CLK->TIM_CLOCK = (" + TIM_BRG);
+						g().addCodeStr(oldCode, " /* разешение тактирования Таймера 1 */");
+						g().addCodeStr(oldCode, "                            |(1 << 24)); ");
+						break;
+					case 2:
+						g().addCodeStr(oldCode, " /* делитель тактовой частоты Таймера 2 */");
+						g().addCodeStr(oldCode, "    MDR_RST_CLK->TIM_CLOCK = ((" + TIM_BRG + " << 8)");
+						g().addCodeStr(oldCode, " /* разешение тактирования Таймера 2 */");
+						g().addCodeStr(oldCode, "                            | (1 << 25)); ");
+						break;
+					case 3:
+						g().addCodeStr(oldCode, " /* делитель тактовой частоты Таймера 3 */");
+						g().addCodeStr(oldCode, "    MDR_RST_CLK->TIM_CLOCK = ((" + TIM_BRG + " << 16)");
+						g().addCodeStr(oldCode, " /* разешение тактирования Таймера 3 */");
+						g().addCodeStr(oldCode, "                            | (1 << 26)); ");
+						break;
+				}
 
                 /* 4 строка */
-		g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->PSG = 0x" + (TIM_PSG - 1) + ";");
+				g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->PSG = 0x" + (TIM_PSG - 1) + ";");
 
                 /* 5 строка */
-		if (TIM_ARR > 65536) {
-			g().addCodeStr(oldCode, "// ЗНАЧЕНИЕ НЕ ДОЛЖНО ПРЕВЫШАТЬ 65536!!! ИЗМЕНИТЕ ВВЕДЕННЫЕ ЗНАЧЕНИЯ!!!");
-		}
-		g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->ARR = 0x" + TIM_ARR + ";");
+				if (TIM_ARR > 65536) {
+					g().addCodeStr(oldCode, "// ЗНАЧЕНИЕ НЕ ДОЛЖНО ПРЕВЫШАТЬ 65536!!! ИЗМЕНИТЕ ВВЕДЕННЫЕ ЗНАЧЕНИЯ!!!");
+				}
+				g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->ARR = 0x" + TIM_ARR + ";");
                 /* 6 строка */
-		g().addCodeStr(oldCode, "//разрешение прерывания по совпадению");
-		g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->IE = (1 << 1);");
+				g().addCodeStr(oldCode, "//разрешение прерывания по совпадению");
+				g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->IE = (1 << 1);");
 
                 /* 7 строка */
-		g().addCodeStr(oldCode, "//счет вверх по TIM_CLK, таймер вкл.");
-		g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->CNTRL = 0x01;");
+				g().addCodeStr(oldCode, "//счет вверх по TIM_CLK, таймер вкл.");
+				g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->CNTRL = 0x01;");
 
                 /* 8 строка */
-		g().addCodeStr(oldCode, "    NVIC_EnableIRQ(Timer" + TIM_CLK_EN + "_IRQn);");
+				g().addCodeStr(oldCode, "    NVIC_EnableIRQ(Timer" + TIM_CLK_EN + "_IRQn);");
 
-                /* 9 строка */
-		g().addCodeStr(oldCode, "//void Timer_Init");
-
+				break;
+			case 1:
                 /* 10 строка */
-		g().addCodeStr(oldCode, "//Прерывание ( " + timTim + " " + timUnit + " )");
-		g().addCodeStr(oldCode, "void ");
-		g().addCodeStr(oldCode, "TIMER" + TIM_CLK_EN + "_IRQn( ");
-		g().addCodeStr(oldCode, "void ");
-		g().addCodeStr(oldCode, " ){");
-
+				g().addCodeStr(oldCode, "//Прерывание ( " + timTim + " " + timUnit + " )");
                 /* 11 строка */
-		g().addCodeStr(oldCode, "    //Исполняемый код");
-
+				g().addCodeStrR(oldCode, "    //Исполняемый код");
                 /* 12 строка */
-		g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->CNT = 0x0000;");
-		g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->STATUS &= ~(1 << 1);");
-		g().addCodeStr(oldCode, "    NVIC_ClearPendingIRQ(Timer" + TIM_CLK_EN + "_IRQn);");
-		return super.generateSimpleCodeStep(oldCode, codeStep);
+				g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->CNT = 0x0000;");
+				g().addCodeStr(oldCode, "    MDR_TIMER" + TIM_CLK_EN + "->STATUS &= ~(1 << 1);");
+				g().addCodeStr(oldCode, "    NVIC_ClearPendingIRQ(Timer" + TIM_CLK_EN + "_IRQn);");
+				g().addCodeStrL(oldCode, "");
+				break;
+		}
+		return oldCode;
+	}
+
+	@Override
+	protected String[] methodNames() {
+		return new String[]{"TMR", "IRQ"};
+	}
+
+	@Override
+	protected List<String> generateCode(Device device, List<String> oldCode, String methodName) {
+		generateCode(oldCode, methodName.equals("TMR") ? 2 : 1);
+		return super.generateCode(device, oldCode, methodName);
 	}
 }
