25e68d4909730cca2feb457a9b7f7a179e3bb2a2
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-22 11:43:50 +0300

    create ControllerTestCase

---
+++ test/milandr_ex/model/ControllerTestCase.java
@@ -0,0 +1,92 @@
+package milandr_ex.model;
+
+import com.google.common.collect.Maps;
+import milandr_ex.data.Device;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Entity contains params and fields for testing cases
+ * Created by lizard on 22.03.17 at 10:59.
+ */
+public class ControllerTestCase {
+	private Device.EPairNames pair;
+	private Map<String, String> params = Maps.newHashMap();
+	private String expectedCodeResult;
+	private int expectedComments;
+	private int expectedLines;
+	private int expectedSize;
+	private String[] paramNames;
+	private String[] paramValues;
+
+	public ControllerTestCase(Device.EPairNames pair) {
+		this.pair = pair;
+	}
+
+	public ControllerTestCase setNames(String[] paramNames) {
+		this.paramNames = paramNames;
+		return this;
+	}
+
+	public ControllerTestCase setValues(String[] paramValues) {
+		this.paramValues = paramValues;
+		return this;
+	}
+
+	public ControllerTestCase build() {
+		for(int i = 0; i < paramNames.length; i++) {
+			this.addParam(paramNames[i], paramValues[i]);
+		}
+		return this;
+	}
+
+	public ControllerTestCase addParam(String name, String value) {
+		params.put(name, value);
+		return this;
+	}
+
+	public String getExpectedCodeResult() {
+		return expectedCodeResult;
+	}
+
+	public ControllerTestCase setExpectedCodeResult(String expectedCodeResult) {
+		this.expectedCodeResult = expectedCodeResult;
+		return this;
+	}
+
+	public int getExpectedComments() {
+		return expectedComments;
+	}
+
+	public ControllerTestCase setExpectedComments(int expectedComments) {
+		this.expectedComments = expectedComments;
+		return this;
+	}
+
+	public int getExpectedLines() {
+		return expectedLines;
+	}
+
+	public ControllerTestCase setExpectedLines(int expectedLines) {
+		this.expectedLines = expectedLines;
+		return this;
+	}
+
+	public int getExpectedSize() {
+		return expectedSize;
+	}
+
+	public ControllerTestCase setExpectedSize(int expectedSize) {
+		this.expectedSize = expectedSize;
+		return this;
+	}
+
+	public void test(BasicControllerTest controller, List<String> codeList) {
+		test(controller, codeList, expectedCodeResult);
+	}
+	public void test(BasicControllerTest controller, List<String> codeList, String expectedCodeResult) {
+		this.build();
+		controller.testCodeResult(expectedCodeResult, expectedComments, expectedLines, expectedSize, codeList);
+	}
+}
