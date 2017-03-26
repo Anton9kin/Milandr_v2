b10dc904f62e51160bea9e56d1646fe24cdabbba
Lizard2k1 <Lizard2k1@gmail.com>
2017-03-26 13:20:19 +0300

    make gpio code comments with listing of selected pins, ports and configuration groups

--- src/milandr_ex/model/mcu/ext/MCUGpioController.java
+++ src/milandr_ex/model/mcu/ext/MCUGpioController.java
@@ -1,14 +1,18 @@
 package milandr_ex.model.mcu.ext;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.layout.GridPane;
 import milandr_ex.data.AppScene;
 import milandr_ex.data.Device;
+import milandr_ex.data.McuBlockProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Set;
 
 import static milandr_ex.data.Constants.textToKey;
 
@@ -45,4 +49,43 @@ public class MCUGpioController extends MCUExtPairController {
 				&& item != null && !item.equals("RESET");
 	}
 
+	@Override
+	protected List<String> generateSimpleCodeStep(List<String> oldCode, int codeStep) {
+		List<String> pinList = getPinList();
+		Set<String> pinPorts = Sets.newLinkedHashSet();
+		Set<String> pinConfs = Sets.newLinkedHashSet();
+		for(String pinText: pinList) {
+			g().addCodeStr(oldCode, "//gpio pin selected: %s", pinText);
+			String parts[] = pinText.split("\\s");
+			String pinPort = "Port_Unk";
+			if (parts[0].matches("P[A-F]\\d{1,2}")) {
+				pinPort = "Port_" + parts[0].charAt(1) + "";
+				pinPorts.add(pinPort);
+			}
+			StringBuilder pinConf = new StringBuilder("\n//Conf: \t").append(pinPort);
+			List<McuBlockProperty> group = getDevicePair().model().getGroup(pinText);
+			int count = 0;
+			for(McuBlockProperty prop: group) {
+				Integer intVal = prop.getIntValue();
+				String strVal = prop.getStrValue();
+				String confValue = strVal;
+				switch (prop.getKind()) {
+					case INT: confValue = intVal + ""; break;
+					case LST: confValue = intVal + ""; break;
+					case CMB: confValue = intVal + ""; break;
+					case CHK: confValue = strVal.equals("true") ? "true" : "false"; break;
+				}
+				pinConf.append(String.format("[%s=%s]", prop.getName(), confValue));
+				if (count++ == 3) { count = 0; pinConf.append("\n//\t"); }
+			}
+			pinConfs.add(pinConf.toString());
+		}
+		for(String pinPort: pinPorts) {
+			g().addCodeStr(oldCode, "//gpio port selected: %s", pinPort);
+		}
+		for(String pinConf: pinConfs) {
+			g().addCodeStr(oldCode, "//gpio config selected: %s", pinConf);
+		}
+		return oldCode;
+	}
 }
