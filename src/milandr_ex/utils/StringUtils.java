855a503c405e75d1a0abe16bc7fe2e1be26bafb1
Lizard2k1 <Lizard2k1@gmail.com>
2017-04-01 11:39:28 +0300

    switch text file keyboard layout

--- src/milandr_ex/utils/StringUtils.java
+++ src/milandr_ex/utils/StringUtils.java
@@ -1,5 +1,10 @@
 package milandr_ex.utils;
 
+import milandr_ex.data.Constants;
+
+import java.io.File;
+import java.util.List;
+
 /**
  * Created by lizard on 20.03.17 at 16:07.
  */
@@ -17,5 +22,49 @@ public class StringUtils {
 			if (str.contains(substr)) return true;
 		}
 		return false;
+	}
+
+	static String eng = "qwertyuiop[]asdfghjkl;'zxcvbnm,.?";
+	static String rus = "йцукенгшщзхъфывапролджэячсмитьбю.";
+	public static void main(String[] args) {
+		if (args.length < 1) return;
+		String fname = args[0];
+		List<String> text = Constants.loadTxtStrings(new File(fname));
+		for(String line: text) {
+			String[] words = line.split("\\s");
+			if (words.length < 1) continue;
+			StringBuilder outLine = new StringBuilder();
+			if (line.startsWith("--->")) {
+				System.out.println(line);
+				continue;
+			}
+			String lastWord = "";
+			for(String word: words) {
+				if (word == null) continue;
+				if (word.isEmpty()) continue;
+				if (word.equals(lastWord)) continue;
+				lastWord = word;
+				if (word.matches("\\[.+]")) {
+					outLine.append(word);
+					continue;
+				}
+				outLine.append(word);
+//                outLine.append(parseWord(word));
+			}
+			if (outLine.toString().trim().isEmpty()) continue;
+			System.out.println(outLine);
+		}
+	}
+
+	private static String parseWord(String word) {
+		String outWord = word;
+		for(int i = 0; i < eng.length(); i++) {
+			String regex = eng.charAt(i) + "";
+			if (!word.contains(regex)) continue;
+			if (regex.equals("[") || regex.equals(".")
+					|| regex.equals("?")) regex = "\\" + regex;
+			outWord = outWord.replaceAll(regex, "" + rus.charAt(i)) + " ";
+		}
+		return outWord.replaceAll("\\s+", " ");
 	}
 }
